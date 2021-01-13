669c98ea42a4d239d63eefdccf54d9692a5e496c
Tyler Arnold <arnoldTyler1998@gmail.com>
2021-01-13 14:27:25 +0000

    journals in state now

--- state/ImageContainer.tsx
+++ state/ImageContainer.tsx
@@ -9,6 +9,12 @@ export interface ImageData {
   description: string;
 }
 
+export interface JournalData {
+  name: string;
+  description: string;
+  images: ImageData[] | undefined;
+}
+
 interface UseImageInterface {
   images: ImageData[] | undefined;
   addImage: (image: ImageData) => void;
@@ -23,24 +29,16 @@ interface UseImageInterface {
  */
 function useImage(): UseImageInterface {
   const [images, setImages] = useState<ImageData[] | undefined>();
+  const [journals, setJournals] = useState<JournalData[]>();
 
-  /**
-   * Adds an image to state
-   * @param {ImageData} image
-   */
-  const addImage = (image: ImageData): void => {
-    images ? setImages([...images, image]) : setImages([image]);
-  };
+  const {addImage, removeImage} = imageInteractions(images, setImages);
 
-  /**
-   * Removes an image
-   * @param {ImageData} image
-   * @return {void |
-   *  React.Dispatch<React.SetStateAction<ImageData[] | undefined>>}
-   */
-  const removeImage = (image: ImageData) => {
-    return images ? setImages(images.filter((i) => i !== image)) : setImages;
-  };
+  const {
+    addJournal,
+    removeJournal,
+    addImgToJournal,
+    removeImgFromJournal,
+  } = journalInteractions(journals, setJournals);
 
   // ignore this warning because our mock api refreshes on a long timer
   // see https://github.com/facebook/react-native/issues/12981
@@ -59,3 +57,112 @@ function useImage(): UseImageInterface {
 }
 
 export const ImageContainer = createContainer(useImage);
+
+/**
+ * Handles basic interactions for journals
+ *
+ * @param {(JournalData[] | undefined)} journals
+ * @param {*} setJournals
+ */
+function journalInteractions(
+    journals: JournalData[] | undefined,
+    setJournals: React.Dispatch<React.SetStateAction<JournalData[] | undefined>>,
+) {
+  /**
+   * Adds a journal
+   * @param {JournalData} journal
+   * @return {void}
+   */
+  const addJournal = (journal: JournalData) => {
+    return journals
+      ? setJournals([...journals, journal])
+      : setJournals([journal]);
+  };
+
+  /**
+   * Removes a journal
+   * @param {JournalData} journal
+   * @return {void | null}
+   */
+  const removeJournal = (journal: JournalData) => {
+    return journals
+      ? setJournals([...journals.filter((j) => j !== journal)])
+      : null;
+  };
+
+  /**
+   * Adds an image to a journal. This is bad code.
+   * @param {JournalData} journal
+   * @param {ImageData} image
+   * @return {void | null}
+   */
+  const addImgToJournal = (journal: JournalData, image: ImageData) => {
+    return journals
+      ? setJournals([
+        ...journals.map((j) =>
+            j === journal
+              ? {...j, images: j.images ? j.images.concat([image]) : [image]}
+              : j,
+        ),
+      ])
+      : null;
+  };
+
+  /**
+   * Removes an image to a journal. This is worse code.
+   * @param {JournalData} journal
+   * @param {ImageData} image
+   * @return {void | null}
+   */
+  const removeImgFromJournal = (journal: JournalData, image: ImageData) => {
+    return journals
+      ? setJournals([
+        ...journals.map((j) =>
+            j === journal
+              ? {
+                ...j,
+                images: j.images
+                    ? j.images.filter((i) => i !== image)
+                    : [image],
+              }
+              : j,
+        ),
+      ])
+      : null;
+  };
+
+  return {addJournal, removeJournal, addImgToJournal, removeImgFromJournal};
+}
+
+/**
+ * Handles basic interactions for images
+ *
+ * @param {(ImageData[] | undefined)} images
+ * @param {*} setImages
+ * @return {*}
+ */
+function imageInteractions(
+    images: ImageData[] | undefined,
+    setImages: React.Dispatch<React.SetStateAction<ImageData[] | undefined>>,
+) {
+  /**
+   * Adds an image to state
+   * @param {ImageData} image
+   * @return {void}
+   */
+  const addImage = (image: ImageData) => {
+    return images ? setImages([...images, image]) : setImages([image]);
+  };
+
+  /**
+   * Removes an image
+   * @param {ImageData} image
+   * @return {void |
+   *  React.Dispatch<React.SetStateAction<ImageData[] | undefined>>}
+   */
+  const removeImage = (image: ImageData) => {
+    return images ? setImages(images.filter((i) => i !== image)) : setImages;
+  };
+
+  return {addImage, removeImage};
+}
